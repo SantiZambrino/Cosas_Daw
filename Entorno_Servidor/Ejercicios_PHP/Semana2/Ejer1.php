9c828848abbd391d0ac4190224cad56fa8ca8523
Santi Zambrino <szo0001@alu.medac.es>
2021-09-27 11:25:29 +0200

    Pasando las cosas de daw

---
+++ Entorno_Servidor/Ejercicios_PHP/Semana2/Ejer1.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+    //Ejer2
+    // $hola = 'Hola mundo';
+    // echo $hola;
+
+    // //Crear variables Ejer3
+    // $entero = 5;
+    // $deci = 1.5;
+    // $cadena = 'Hola que tal';
+
+    // var_dump ($entero, $deci, $cadena);
+
+    // //Ejer4
+    // $binario = true;
+    // $vacio = null;
+    
+    // var_dump($binario, $vacio);
+
+    //Ejer5
+    // $num1 = 2;
+    // $num2 = 6;
+
+    // $suma = $num1 + $num2;
+    // $resta = $num2 - $num1;
+    // $multiplica = $num1 * $num2;
+    // $divide = $num2 / $num1;
+    // $resto = $num2 % $num1;
+    // $potencia = pow($num1, $num2);
+    // var_dump($suma, $resta, $multiplica, $divide, $resto, $potencia);
+    // printf($suma);
+
+    // //Ejer6
+
+    // $palabra = 'Hola';
+    // $palabra2 = ' Nala';
+
+    // printf($palabra.$palabra2);
+
+    //Ejer7
+    $cadena = '1';
+    var_dump($cadena);
+
+    $multiplica_string = $cadena * 2;
+
+    var_dump($multiplica_string);
+
+    $multiplica_string = $cadena * 1.5;
+
+    var_dump($multiplica_string);
+
+    $multiplica_string = $multiplica_string + '10 manzanas';
+
+    var_dump($multiplica_string);
+
+    // $cadena =  $cadena + '10 manzanas';
+
+    // $multiplica_manzana = $cadena * 1.5;
+
+    //Ejer8
+    // $buleano1 = true;
+    // $buleano2 = false;
+
+    // printf($buleano1 and $buleano2);
+
+?>
+</body>
+</html>
+
---
+++ Entorno_Servidor/Tema2/Cincouno.php
@@ -0,0 +1,18 @@
+<?php
+    // En php.ini se configura php
+
+    //Devuelve informacion general sobre PHP. Ejemplo la ubicacion exacta del fichero de configuracion php.ini
+    phpinfo();
+
+    //Variables
+    //Siempre con el simbolo del dolar
+    $var= 'hola';
+
+    //Constante
+    define("num", 15);
+
+    //Sacar por pantalla
+    echo num;
+
+    //
+?>
