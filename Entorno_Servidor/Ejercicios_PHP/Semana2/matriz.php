6072224dd13781d0c837a62a9f7d8fad23e7f9f9
Santi Zambrino <szo0001@alu.medac.es>
2021-09-28 13:03:56 +0200

    otro ejercicio

---
+++ Entorno_Servidor/Ejercicios_PHP/Semana2/matriz.php
@@ -0,0 +1,20 @@
+<pre>
+<?php
+    $matrizMatriculas = array(
+        array(
+            'id'=> 1,
+            'enrollment'=>"1234ABC"
+        ),
+        array(
+            'id'=> 2,
+            'enrollment'=>"2345FEC"
+        ),
+        array(
+            'id'=> 3,
+            'enrollment'=>"542GHK"
+        )
+    );
+
+    print_r($matrizMatriculas);
+ ?>
+ </pre>
