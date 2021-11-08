ed230fb11e24d2ebd5fb2aebfeca4c9dd03fc388
Santi Zambrino <santizambrino@gmail.com>
2021-11-08 22:10:06 +0100

    js en casa

---
+++ Entorno_Cliente/Clases_de_verdad/ejercicios/Acceso_a_Nodo/dieciocho/script.js
@@ -0,0 +1,40 @@
+var timer;
+var salto = 2;
+var saltoMeter = 2; 
+var sumaMonedas = 1;
+var sumatotal = 0;
+
+window.onload = function () {
+    var barraProgreso = document.getElementsByTagName("button")[0];
+    var botonMeter = document.getElementsByTagName("button")[1];
+    barraProgreso.onclick = function () {
+        barraProgreso.disabled = true;
+        timer = setInterval("cargar()", 200);
+
+    }
+    botonMeter.onclick = function() {
+        botonMeter.disabled = true;
+        timer = setInterval("descargar()", 200);
+    }
+}
+
+function cargar() {
+
+    var barraProgreso = document.getElementsByTagName("progress")[0];
+    var botonProgreso = document.getElementsByTagName("button")[0];
+    var moneda = document.getElementsByClassName("monedas")[0];
+   
+    console.log(barraProgreso.value);
+    
+    if (barraProgreso.value < barraProgreso.max) {
+        barraProgreso.value = salto;
+        salto += salto;
+    }else {  
+        barraProgreso.value = barraProgreso.max;
+        // parseInt(moneda.innerHTML) +=  sumaMonedas;  
+        console.log("valor final: " + barraProgreso.value);
+        clearInterval(timer);
+        botonProgreso.disabled = false;
+    }
+
+}
--- Entorno_Cliente/Clases_de_verdad/ejercicios/Acceso_a_Nodo/dieciséis/script.js
+++ Entorno_Cliente/Clases_de_verdad/ejercicios/Acceso_a_Nodo/dieciséis/script.js
@@ -1,49 +1,51 @@
 /*
 Usando las funciones setInterval y clearInterval de Javascript. Coloca una etiqueta meter, una etiqueta progress y un botón al lado de cada una. Al pulsar el botón se debe simular la “carga” de la barrita. Muestra un mensaje cuando se alcance elmáximo.
+
+18)
+Usa   sólo   el   elemento   progress   y   el   botón   que   se   encarga   de   realizar   el funcionamiento explicado en el ejercicio 16. Cambia el nombre del botón por“Generar moneda”
 */
 
 
 var timer;
 var salto = 2;
-var saltoMeter = 2; 
 
 window.onload = function () {
-    var botonCarga = document.getElementsByTagName("button")[0];
+    var barraProgreso = document.getElementsByTagName("button")[0];
     var botonMeter = document.getElementsByTagName("button")[1];
-    botonCarga.onclick = function () {
-        botonCarga.disabled = true;
+    barraProgreso.onclick = function () {
+        barraProgreso.disabled = true;
         timer = setInterval("cargar()", 200);
-        botonCarga.disabled = false;
+
     }
     botonMeter.onclick = function() {
         botonMeter.disabled = true;
         timer = setInterval("descargar()", 200);
-        if (condition) {
-            
-        }
-        botonMeter.disabled = false;
     }
 }
 
 function cargar() {
-    var botonCarga = document.getElementsByTagName("progress")[0];
-    console.log(botonCarga.value);
-    if (botonCarga.value < botonCarga.max) {
-        botonCarga.value = salto;
+    var barraProgreso = document.getElementsByTagName("progress")[0];
+    var barraProgresor = document.getElementsByTagName("button")[0];
+    console.log(barraProgreso.value);
+    if (barraProgreso.value < barraProgreso.max) {
+        barraProgreso.value = salto;
         salto += salto;
     }else{
-        botonCarga.value = botonCarga.max;
-        console.log("valor final: " + botonCarga.value);
+        barraProgreso.value = barraProgreso.max;
+        console.log("valor final: " + barraProgreso.value);
         clearInterval(timer);
+        barraProgresor.disabled = false;
     }
 }
 
 function descargar() {
     var meter = document.getElementsByTagName("meter")[0];
+    var botonMeter = document.getElementsByTagName("button")[1];
     if (meter.value > meter.min) {
         meter.value -= saltoMeter;
     } else{
         console.log("valor final: " + meter.value);
         clearInterval(timer);
+        botonMeter.disabled = false;
     }
 }
