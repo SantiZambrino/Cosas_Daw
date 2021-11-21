c367813ffbc5da4c2405a260889cc64591d17cb2
Santi Zambrino <santizambrino@gmail.com>
2021-11-21 20:31:26 +0100

    Ascensor en casa

--- pasar_tiempo/ascensor/jscode/funciones.js
+++ pasar_tiempo/ascensor/jscode/funciones.js
@@ -10,6 +10,12 @@ window.onload = ()=>{
     let papi;
     let botones;
     let spanes;
+    let eleccionPlantaUsu;
+    let mensaje;
+    let recorrerPlantas;
+    let estilosPlantas;
+    let fondo;
+    let contador = 5;
     // do {
     //     if (plantasTotales < 1 || plantasTotales > 8)
     //     alert('ERROR NUMERO SUPERIOR A 8 O INFERIOR A 0');
@@ -24,24 +30,67 @@ window.onload = ()=>{
    botones[0].disabled = true;
    botones[1].disabled = true;
    //Creo las plantas con la forma b;
-    for (let i = plantasTotales; i > 0; i--) {
-        plantas = document.createElement('div');
+    // for (let i = plantasTotales; i > 0; i--) {
+    //     plantas = document.createElement('div');
 
-        if (i == 1) 
-        plantas.style.border = '4px solid black',
+    //     if (i == 1) 
+    //     // plantas.style.border = '4px solid black',
+    //     plantas.style.setProperty('border', '4px solid black'),
+    //     plantas.style.backgroundColor = 'yellow',
+    //     spanes = document.getElementsByTagName('span'),
+    //     spanes[0].innerHTML = `${i}`,
+    //     spanes[1].innerHTML = 'Planta',
+    //     botones[0].disabled = false;
+
+    //     plantas.className = 'planta';
+
+    //     nodoTexto = document.createTextNode(i);
+    //     plantas.appendChild(nodoTexto);
+    //     papi = document.getElementById('ascensor');
+    //     papi.appendChild(plantas);
+    // }
+
+    for (let i = 0; i < plantasTotales; i++) {
+        plantas = document.createElement('div');
+        if (i == 4) 
+        // plantas.style.border = '4px solid black',
+        plantas.style.setProperty('border', '4px solid black'),
         plantas.style.backgroundColor = 'yellow',
         spanes = document.getElementsByTagName('span'),
-        spanes[0].innerHTML = `${i}`,
+        spanes[0].innerHTML = `${contador}`,
         spanes[1].innerHTML = 'Planta',
         botones[0].disabled = false;
 
         plantas.className = 'planta';
 
-        nodoTexto = document.createTextNode(i);
+        nodoTexto = document.createTextNode(contador);
         plantas.appendChild(nodoTexto);
         papi = document.getElementById('ascensor');
         papi.appendChild(plantas);
+        contador--;
     }
-    
+    /*
+    Si la planta a la que debe ir el ascensor es la misma que la planta en la que está, mostrará un mensaje por pantalla diciendo: Ya estás en esa planta.
+    */
+    botones[0].onclick = ()=>{
+        mensaje = 'Elija la planta a la que quiera ir.';
+        mensaje += `\nOpciones desde la 1º planta hasta la ${plantasTotales}º planta: `;
+        eleccionPlantaUsu = parseInt(prompt(mensaje));
 
-}
+        if (eleccionPlantaUsu < 1) 
+        alert(`La planta ${eleccionPlantaUsu}º no existe ira a la 1º planta`),
+        eleccionPlantaUsu = 1;
+        else if (eleccionPlantaUsu > plantasTotales)
+        alert(`La planta ${eleccionPlantaUsu}º no existe ira a la ${plantasTotales}º planta`),
+        eleccionPlantaUsu = plantasTotales;
+
+        // recorrerPlantas = document.getElementsByClassName('planta');
+        // estilosPlantas = window.getComputedStyle(recorrerPlantas[eleccionPlantaUsu -1]);
+        // fondo = estilosPlantas.getPropertyValue('background-Color');
+        // console.log({fondo});
+        // if (fondo == "rgb(255, 255, 0)") {
+        //     alert ('estas en la misma planta');
+        // }
+    
+        }
+    }
