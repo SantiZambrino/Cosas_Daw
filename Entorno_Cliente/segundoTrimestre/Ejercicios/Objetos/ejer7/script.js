87499cb8180e6fd245d700057052e8129ff8ad57
Santi Zambrino <szo0001@alu.medac.es>
2021-12-16 14:38:24 +0100

    cosas jueves

---
+++ Entorno_Cliente/segundoTrimestre/Ejercicios/Objetos/ejer7/script.js
@@ -0,0 +1,23 @@
+const usuario = {
+    genero: true,
+    datos ={
+        name: "santi",
+        lastname: "Zambrino",
+        age: 33,
+        Dni: "79158346Z" 
+    },
+
+    adress ={
+        street: "Calle Pepe Jose",
+        number: 12,
+        city:"Málaga",
+        country: "Spain",
+    },
+    coordenates = {
+        latitude: 2134241,
+        longitude: 4435245,
+    },
+    login={
+        id: "a1b2c3d4"
+    }
+}
