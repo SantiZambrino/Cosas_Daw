c86be0ca28dfd67bf45279d3f3c8de4a999301ce
Santi Zambrino <szo0001@alu.medac.es>
2022-01-20 14:41:00 +0100

    7 clientes

---
+++ Entorno_Cliente/segundoTrimestre/Ejercicios/Formulario/7/script.js
@@ -0,0 +1,9 @@
+/*
+Cada vez que se pulse el botón
+Publicar, se va a crear un div cuyo contenido será el texto del textarea. Nota: maqueta de
+manera vistosa los DIV creados
+*/
+
+window.onload = ()=>{
+
+}
