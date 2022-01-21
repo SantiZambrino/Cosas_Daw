4e039776fba991253278f3269c0f23ea5ed6cbfe
Santi Zambrino <santizambrino@gmail.com>
2022-01-21 20:02:46 +0100

    7 terminado

--- Entorno_Cliente/segundoTrimestre/Ejercicios/Formulario/7/script.js
+++ Entorno_Cliente/segundoTrimestre/Ejercicios/Formulario/7/script.js
@@ -7,13 +7,18 @@ manera vistosa los DIV creados
 window.onload = ()=>{
     const f = document.forms[0];
     const btn = f.btn;
+    const textArea = f.comentario;
 
 
     btn.onclick=()=>{
-        console.log(f.comentario)
         let texto = f.comentario.value;
-        console.log(texto)
         publicar(f,texto)
+    }
+
+    //Si al pulsar la tecla mas de 100 veces deja de escribir en el text area
+    textArea.onkeypress=()=>{
+        if(f.comentario.value.length < 100) return true;
+        else return false;
     }
 }
 
@@ -25,10 +30,22 @@ caracteres escritos, no se puede seguir escribiendo. El botón Publicar no debe 
 su comportamiento
 */
 function publicar(f, texto) {
+    console.log(texto.length)
+    //limitar a 50 caracteres
+    let nuevoTexto = ""
+    let cantidadLetras = 0;
+    for (const letra of texto) {
+        if (cantidadLetras < 50) {
+           nuevoTexto += letra;
+        }
+        cantidadLetras++;
+    }
+    console.log(nuevoTexto.length)
+    // console.log({nuevoTexto});
     const padre = document.querySelector('body');
     let nuevo = document.createElement('div')
     nuevo.className = "divClase";
-    let nodoTexto = document.createTextNode(texto);
+    let nodoTexto = document.createTextNode(texto);// texto codigo normal, nuevoTexto limitado a 50
     nuevo.appendChild(nodoTexto);
     padre.appendChild(nuevo);
 }
