0a3fd075b9fb42a00289cdd0a93a6a3038340a6e
Santi Zambrino <santizambrino@gmail.com>
2022-01-25 21:04:06 +0100

    clientes

--- Entorno_Cliente/segundoTrimestre/Ejercicios/Objetos/ejer7/script2.js
+++ Entorno_Cliente/segundoTrimestre/Ejercicios/Objetos/ejer7/script2.js
@@ -1,64 +1,106 @@
 const usuario = {
-    genre: "masculino",
-    datos: {
-      name: "Prueba",
-      lastname: "Prueba",
-      age: 25,
-      dni: "123456789J",
-    },
-    address: {
-      street: "C/ Prueba",
-      number: 4,
-      city: "málaga",
-      country: "españa",
-    },
-    coordenates: {
-      latitud: 36.7196,
-      longitud: -4.42002,
-    },
-    login: {
-      id: "6622",
-      user: "pruebaPrueba",
-      pass: "123456789",
-      picture: "img/avatarSmall.png",
-    },
-    contact: {
-      phone: 666666666,
-      email: "prueba@gmail.com",
-      ssnn: ["linkedin", "github", "twitter"],
-    },
-    pictures: {
-      big: "img/avatarBig.png",
-      small: "img/avatarSmall.png",
-    },
-    softwareSkills: [
-      "Databases",
-      "Graphic Design",
-      "Computer Programming",
-      "Presentation Tools",
-      "Social Media",
-    ],
-  };
+  genre: "masculino",
+  datos: {
+    name: "Prueba",
+    lastname: "Prueba",
+    age: 25,
+    dni: "123456789J",
+  },
+  address: {
+    street: "C/ Prueba",
+    number: 4,
+    city: "málaga",
+    country: "españa",
+  },
+  coordenates: {
+    latitud: 36.7196,
+    longitud: -4.42002,
+  },
+  login: {
+    id: "6622",
+    user: "pruebaPrueba",
+    pass: "123456789",
+    picture: "img/avatarSmall.png",
+  },
+  contact: {
+    phone: 666666666,
+    email: "prueba@gmail.com",
+    ssnn: ["linkedin", "github", "twitter"],
+  },
+  pictures: {
+    big: "img/avatarBig.png",
+    small: "img/avatarSmall.png",
+  },
+  softwareSkills: [
+    "Databases",
+    "Graphic Design",
+    "Computer Programming",
+    "Presentation Tools",
+    "Social Media",
+  ],
+};
 
-
-    //Recorro el objeto
-    for (const campoUsuario in usuario) {
-        //Compruebo si es objeto
-        if (typeof usuario[campoUsuario] === "object")
-           //Si lo es compruebo si es array 
-          if (Array.isArray(usuario[campoUsuario]))
-            //Si lo es lo muestro
-            console.log(`${campoUsuario}: ${usuario[campoUsuario]}`);
-          else {
-              //Si no es array recorro el objeto y lo muestro
-            for (let campoInterno in usuario[campoUsuario]) {
-              console.log(
-                `${campoInterno}: ${usuario[campoUsuario][campoInterno]}`
-              );
-            }
+window.onload = () => {
+  const padre = document.getElementsByClassName("info-usu")[0];
+  //Recorro el objeto
+  for (const campoUsuario in usuario) {
+    // console.log(campoUsuario +"tiene"+ usuario[campoUsuario])
+    //Compruebo si es objeto
+    if (typeof usuario[campoUsuario] === "object")
+      if (Array.isArray(usuario[campoUsuario])) {
+        //Si lo es compruebo si es array
+        //Si lo es lo muestro
+        let texto = `${campoUsuario}: ${usuario[campoUsuario]}`;
+        console.log({texto})
+        crearArray(texto, padre)
+      } else {
+        //Si no es array recorro el objeto y lo muestro
+        for (let campoInterno in usuario[campoUsuario]) {
+          let texto = `${campoInterno}: ${usuario[campoUsuario][campoInterno]}`;
+          switch (campoInterno) {
+            case "user":
+              crear(texto, padre);
+              break;
+            case "name":
+              crear(texto, padre);
+              break;
+            case "lastname":
+              crear(texto, padre);
+              break;
+            case "age":
+              crear(texto, padre);
+              break;
+            case "street":
+              crear(texto, padre);
+              break;
+            case "phone":
+              crear(texto, padre);
+              break;
+            case "email":
+              crear(texto, padre);
+              break;
           }
-          //Si no es objeto lo muestro
-        else{
-            console.log(`${campoUsuario}: ${usuario[campoUsuario]}`);
-        }  
+        }
       }
+    //Si no es objeto lo muestro
+    else {
+      // console.log(`${campoUsuario}: ${usuario[campoUsuario]}`);
+    }
+  }
+};
+
+function crear(texto, padre) {
+  let nuevo = document.createElement("p");
+  let textoAcopiar = texto;
+  let nodotexto = document.createTextNode(textoAcopiar);
+  nuevo.appendChild(nodotexto);
+  padre.appendChild(nuevo);
+}
+function crearArray(texto, padre) {
+  let nuevo = document.createElement("p");
+  nuevo.id ='softwareSkills';
+  let textoAcopiar = texto;
+  let nodotexto = document.createTextNode(textoAcopiar);
+  nuevo.appendChild(nodotexto);
+  padre.appendChild(nuevo);
+}
